3e5a4a7097f07da19b29fa922e708d4c52922f26
Doltons Edward Nicholas <75367447+doltonsedward@users.noreply.github.com>
2022-03-03 21:09:02 +0800

    use return array in function, Go Basic

--- Go Basic/function.go
+++ Go Basic/function.go
@@ -2,12 +2,18 @@ package main
 
 import "fmt"
 
-func getFullName() {
+func getData() [3]string {
 	fullName := [...]string{
 		"Doltons",
 		"Edward",
 		"Nicholas",
 	}
+
+	return fullName
+}
+
+func getFullName() {
+	fullName := getData()
 
 	var result string
 	for i := 0; i < len(fullName); i++ {
@@ -17,9 +23,9 @@ func getFullName() {
 	fmt.Printf("%s, %d length of name", result, len(result))
 }
 
-func sayHelloTo(firstName string, lastName string) {
+func sayHelloTo(firstName string, lastName string) string {
 	fullName := firstName + " " + lastName
-	fmt.Println("Hello", fullName)
+	return "Hello " + fullName
 }
 
 func main() {
@@ -27,5 +33,5 @@ func main() {
 
 	getFullName()
 	fmt.Printf("\n")
-	sayHelloTo("Eka", "Dwi")
+	fmt.Println(sayHelloTo("Eka", "Dwi"))
 }
